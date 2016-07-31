7d94677417a20dab0fc2d7c928ed880ba9cd72ae
Denis <denis.balashevich@gmail.com>
2016-07-31 22:52:04 +0300

    up

---
+++ BookStorage/SerializeBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStorage
+{
+    public class SerializeBook : IBookListStorage
+    {
+        private string filename;
+
+
+        public SerializeBook(string filename)
+        {
+            this.filename = filename;
+        }
+
+
+        public void SaveBooks(IEnumerable<Book> items)
+        {
+            FileMode fileMode = File.Exists(filename) ? FileMode.Open : FileMode.Create;
+            var formatter = new BinaryFormatter();
+            using (var s = new FileStream(filename, fileMode))
+            {
+                formatter.Serialize(s, items);
+            }
+
+        }
+
+        List<Book> IBookListStorage.LoadBooks()
+        {
+            var books = new List<Book>();
+            var formatter = new BinaryFormatter();
+            using (var s = new FileStream(filename, FileMode.Open))
+            {
+                books = (List<Book>)formatter.Deserialize(s);
+            }
+            return books;
+        }
+    }
+}
---
+++ BookStorage/XMLBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BookStorage
+{
+    class XMLBook: IBookListStorage
+    {
+        private string filename;
+
+        public XMLBook(string filename)
+        {
+            this.filename = filename;
+        }
+
+
+
+        public void SaveBooks(IEnumerable<Book> items)
+        {
+            var xml = new XElement("Books",
+                items.Select((book) => new XElement("Book",
+                    new XElement("author", book.Author),
+                    new XElement("title", book.Title),
+                    new XElement("year", book.Year),
+                    new XElement("pages", book.PagesNumber))));
+            xml.Save(filename);
+        }
+
+        List<Book> IBookListStorage.LoadBooks()
+        {
+            var books = new List<Book>();
+            if (!File.Exists(filename))
+                return books;
+            var document = XDocument.Load(filename);
+            foreach (var book in document.Root.Elements("Book"))
+            {
+                books.Add(new Book(book.Element("author").Value, book.Element("title").Value, int.Parse(book.Element("year").Value), int.Parse(book.Element("pages").Value)));
+            }
+            return books;
+        }
+    }
+}
